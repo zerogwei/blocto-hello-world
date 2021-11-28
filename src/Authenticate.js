26415652152190323f1ae975d15fcc49dd03aa06
zerogwei <78648012+zerogwei@users.noreply.github.com>
2021-11-28 16:12:38 +1100

    first commit

---
+++ src/Authenticate.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from "react"
+import styled from "styled-components"
+import * as fcl from "@onflow/fcl"
+
+const Card = styled.div`
+  margin: 10px 5px;
+  padding: 10px;
+  border: 1px solid #c0c0c0;
+  border-radius: 5px;
+`
+
+const SignInOutButton = ({ user: { loggedIn } }) => {
+  const signInOrOut = async (event) => {
+    event.preventDefault()
+
+    if (loggedIn) {
+      fcl.unauthenticate()
+    } else {
+      fcl.authenticate()
+    }
+  }
+
+  return (
+    <button onClick={signInOrOut}>
+      {loggedIn ? 'Sign Out' : 'Sign In/Up'}
+    </button>
+  )
+}
+
+const CurrentUser = () => {
+  const [user, setUser] = useState({})
+
+  useEffect(() =>
+    fcl
+      .currentUser()
+      .subscribe(user => setUser({...user}))
+  , [])
+
+  return (
+    <Card>
+      <SignInOutButton user={user} />
+    </Card>
+  )
+}
+
+export default CurrentUser
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as fcl from "@onflow/fcl"
+import './index.css'
+import App from './App'
 import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './serviceWorker'
+
+fcl.config()
+  .put("accessNode.api", "https://access-testnet.onflow.org") // connect to Flow testnet
+  .put("challenge.handshake", "https://flow-wallet-testnet.blocto.app/authn") // use Blocto testnet wallet
 
 ReactDOM.render(
   <React.StrictMode>
